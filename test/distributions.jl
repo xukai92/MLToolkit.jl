4c681965d0cf40f9802332c6493a6d66a286e9b7
Kai Xu <xukai921110@gmail.com>
2018-12-31 03:19:42 +0000

    improve test for displaced Poisson

--- test/distributions.jl
+++ test/distributions.jl
@@ -1,8 +1,13 @@
 using MLToolkit, Distributions, Test
 
 @testset "DisplacedPoisson" begin
-    x = 0:1:100
-    y_p = Distributions.pdf.(Ref(Poisson(0.3)), x)
-    y_dpeqv = pdf.(Ref(DisplacedPoisson(0.3, 0.0)), x)
-    @test all(isapprox.(y_p, y_dpeqv))
+    for _ = 1:10
+        λ = rand() * 10
+        x = 0:1:100
+        y_p = Distributions.pdf.(Ref(Poisson(λ)), x)
+        # When r equals 0, displaced Poisson reduces to normal Poisson.
+        # Thus we can check the p.d.f using the Poisson (from Distributions.jl).
+        y_dpeqv = pdf.(Ref(DisplacedPoisson(λ, 0.0)), x)
+        @test all(isapprox.(y_p, y_dpeqv))
+    end
 end
