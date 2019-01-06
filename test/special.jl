0f5093e0e70cb5dcf507edcf0333a70b51e26b7a
Kai Xu <xukai921110@gmail.com>
2019-01-06 22:14:32 +0000

    update test

--- test/special.jl
+++ test/special.jl
@@ -1,23 +1,22 @@
 using MLToolkit, Test
 using MLToolkit: _lbeta, _beta
-using SpecialFunctions: lbeta, beta
 
 @testset "Special" begin
     dims = (10, 5)
 
     @testset "lbeta" begin
         for _ = 1:NUM_RANDTESTS
-            x = randn(dims...).^2
-            y = randn(dims...).^2
+            x = AT(randn(FT, dims...).^2)
+            y = AT(randn(FT, dims...).^2)
             @test _lbeta.(x, y) ≈ lbeta.(x, y)
         end
     end
 
     @testset "beta" begin
         for _ = 1:NUM_RANDTESTS
-            x = randn(dims...).^2
-            y = randn(dims...).^2
-            @test _beta.(x, y) ≈ beta.(x, y) 
+            x = AT(randn(FT, dims...).^2)
+            y = AT(randn(FT, dims...).^2)
+            @test _beta.(x, y) ≈ beta.(x, y)
         end
     end
 end
