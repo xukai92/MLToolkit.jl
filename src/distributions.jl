3fa45636c3b8d11110ce5713c0a8c447e64eb65f
Kai Xu <xukai921110@gmail.com>
2019-01-06 22:36:58 +0000

    remove mckl

--- src/distributions.jl
+++ src/distributions.jl
@@ -1,15 +1,5 @@
 import Distributions: logpdf, pdf, rand, mode
 
-"""
-    mckl(logpdfp, logpdfq, x; args...)
-
-Compute the MC estimate of KL divergence between `p` and `q`.
-"""
-function mckl(p, q, x; args...)
-    return mean(logpdf(p, x; args...) - logpdf(q, x; args...); dims=2)
-end
-mckl(p, q; args...) = mckl(p, q, rand(p); args...)
-
 include("distributions/displaced_poisson.jl")
 include("distributions/ibp.jl")
 include("distributions/normal.jl")
@@ -17,4 +7,4 @@ include("distributions/gumbel.jl")
 include("distributions/bernoulli.jl")
 include("distributions/beta.jl")
 
-export logpdf, pdf, rand, mckl, kl, mode
+export logpdf, pdf, rand, kl, mode
