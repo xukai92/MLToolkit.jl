85e126ed66b925cdc97ec0afb209407e766c0f73
Kai Xu <xukai921110@gmail.com>
2019-01-07 23:09:07 +0000

    merge BatchNormal and BatchNormalLogVar tests

--- test/distributions/normal.jl
+++ test/distributions/normal.jl
@@ -7,7 +7,7 @@ using LinearAlgebra: I
     d = 10
     n = 500_000
 
-    @testset "BatchNormal" begin
+    @testset "BatchNormal(LogVar)" begin
         for _ = 1:NUM_RANDTESTS
             # rand
             μ = randn(FT, d, 1); Σ = randn(FT, d, 1).^2
@@ -17,35 +17,6 @@ using LinearAlgebra: I
 
             @test mean(x; dims=2) ≈ μ atol=(d * ATOL_RAND)
             @test var(x; dims=2) ≈ Σ atol=(d * ATOL_RAND)
-
-            # logpdf
-            mvn = MvNormal(vec(μ), sqrt.(vec(Σ)))
-            x = Matrix{FT}(rand(mvn, n))
-            lp = logpdf(mvn, x)
-
-            @test vec(sum(logpdf(bn, AT(x)); dims=1)) ≈ lp atol=(d * ATOL)
-
-            # kl
-            μ1 = zeros(FT, d, 1); Σ1 = ones(FT, d, 1)
-            μ2 = μ1 .+ rand(FT); Σ2 = Σ1 .* abs(rand(FT))
-
-            mvn1 = MvNormal(vec(μ1), sqrt.(vec(Σ1)))
-            mvn2 = MvNormal(vec(μ2), sqrt.(vec(Σ2)))
-            x = rand(mvn1, n)
-            kl_mc = mean(logpdf(mvn1, x) - logpdf(mvn2, x))
-
-            bn1 = BatchNormal{AT}(μ1, Σ1)
-            bn2 = BatchNormal{AT}(μ2, Σ2)
-            @test sum(kldiv(bn1, bn2)) ≈ kl_mc atol=(d * ATOL_RAND)
-
-            @test sum(kldiv(bn1, BatchNormal(μ2[1,1], Σ2[1,1]))) ≈ kl_mc atol=(d * ATOL_RAND)
-        end
-    end
-
-    @testset "BatchNormalLogVar" begin
-        for _ = 1:NUM_RANDTESTS
-            # rand
-            μ = randn(FT, d, 1); Σ = randn(FT, d, 1).^2
 
             bnlv = BatchNormalLogVar{AT}(μ, log.(Σ))
             x = Array(hcat([rand(bnlv) for _ = 1:n]...))
@@ -58,6 +29,7 @@ using LinearAlgebra: I
             x = Matrix{FT}(rand(mvn, n))
             lp = logpdf(mvn, x)
 
+            @test vec(sum(logpdf(bn, AT(x)); dims=1)) ≈ lp atol=(d * ATOL)
             @test vec(sum(logpdf(bnlv, AT(x)); dims=1)) ≈ lp atol=(d * ATOL)
 
             # kl
@@ -69,14 +41,15 @@ using LinearAlgebra: I
             x = rand(mvn1, n)
             kl_mc = mean(logpdf(mvn1, x) - logpdf(mvn2, x))
 
+            bn1 = BatchNormal{AT}(μ1, Σ1)
             bnlv1 = BatchNormalLogVar{AT}(μ1, log.(Σ1))
             bn2 = BatchNormal{AT}(μ2, Σ2)
-            @test sum(kldiv(bnlv1, bn2)) ≈ kl_mc atol=(d * ATOL_RAND)
+            @test sum(kldiv(bn1, bn2)) ≈ kl_mc atol=(d * ATOL_RAND)
 
+            @test sum(kldiv(bn1, BatchNormal(μ2[1,1], Σ2[1,1]))) ≈ kl_mc atol=(d * ATOL_RAND)
             @test sum(kldiv(bnlv1, BatchNormal(μ2[1,1], Σ2[1,1]))) ≈ kl_mc atol=(d * ATOL_RAND)
         end
     end
-
 
     # NOTE: the test below doesn't include `KnetArray` because the lack of
     #       the support of `det` and `inv` for `KnetArray`.
