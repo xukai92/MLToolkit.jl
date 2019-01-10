22498f66b35155dcd67c0e2d35339c8be27c7c20
Kai Xu <xukai921110@gmail.com>
2019-01-10 02:53:42 +0000

    impl the buffet metaphor for IBP

--- src/distributions/ibp.jl
+++ src/distributions/ibp.jl
@@ -1,4 +1,4 @@
-using Distributions: Beta, Bernoulli
+using Distributions: Beta, Bernoulli, Poisson
 
 struct IBP{T<:Real}
     α::T
@@ -12,11 +12,38 @@ IBP(α::T) where {T<:Real} = IBP{T}(α)
 """
     rand(ibp::IBP, Kmax::Int, n::Int)
 
-Sample from IBP using the stick-breaking construction. The returned sample is of size `n`-by-`Kmax`, which is not a Julia convension by a math convension.
+Sample from IBP using the stick-breaking construction.
+The returned sample is of size `n`-by-`Kmax`,
+which is not a Julia convension by a math convension.
 """
 function rand(ibp::IBP, Kmax::Int, n::Int)
     ν = rand(Beta(ibp.α, 1), Kmax)
     p = break_stick_ibp(ν)
     Z = hcat(rand.(Bernoulli.(p), n)...)
+    return Z
+end
+
+"""
+    rand(ibp::IBP, n::Int)
+
+Sample from IBP using the buffet metaphor.
+The returned sample is of size `n`-by-`Kmax`,
+which is not a Julia convension by a math convension.
+"""
+function rand(ibp::IBP, n::Int)
+    k_init = rand(Poisson(ibp.α))
+    Z = zeros(Integer, n, k_init)
+    Z[1,:] .= 1
+    for i = 2:n
+        for k = 1:size(Z, 2)
+            mk = sum(Z[:,k])
+            Z[i,k] = rand(Bernoulli(mk / i))
+        end
+        k_new = rand(Poisson(ibp.α / i))
+        if k_new > 0
+            Z = hcat(Z, zeros(Integer, n, k_new))
+            Z[i,end-k_new+1:end] .= 1
+        end
+    end
     return Z
 end
--- test/distributions/ibp.jl
+++ test/distributions/ibp.jl
@@ -2,8 +2,11 @@ using Test, MLToolkit
 
 @testset "IBP" begin
     d, n = 10, 5
-
-    ibp = IBP(2.0)
-    Z = rand(ibp, d, n)
-    @test size(Z) == (n, d)
+    for _ = 1:NUM_RANDTESTS
+        ibp = IBP(2.0)
+        Z = rand(ibp, d, n)
+        @test size(Z) == (n, d)
+        Z = rand(ibp, n)
+        @test size(Z, 1) == n
+    end
 end
