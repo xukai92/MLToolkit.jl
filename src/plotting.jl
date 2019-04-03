389d271bfd85b0faf47866f0da2487df3f113f96
Kai Xu <xukai921110@gmail.com>
2019-04-03 15:44:28 +0100

    resolve dep plt["str"] syntax

--- src/plotting.jl
+++ src/plotting.jl
@@ -5,7 +5,7 @@ function plot_two_y_axes(xs, ys1, ys2; color1="tab:red", color2="tab:blue",
     xlabel == nothing || ax1."set_xlabel"(xlabel)
     ylabel1 == nothing || ax1."set_ylabel"(ylabel1, color=color1)
     ax1."tick_params"(axis="y", labelcolor=color1)
-    ax2 = ax1["twinx"]()
+    ax2 = ax1."twinx"()
     ax2."plot"(xs, ys2, c=color2)
     ylabel2 == nothing || ax2."set_ylabel"(ylabel2, color=color2)
     ax2."tick_params"(axis="y", labelcolor=color2)
