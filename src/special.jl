615dedebeffb67324d50e18dacdf847bc1d7016d
Kai Xu <xukai921110@gmail.com>
2019-04-21 21:32:18 +0100

    fix broadcating of beta

--- src/special.jl
+++ src/special.jl
@@ -5,8 +5,9 @@ import SpecialFunctions: lbeta, beta
 # Those in `SpecialFunctions.jl` are implemented for `(::Number, ::Number)` only,
 # for which `KnetArray` cannot broadcast through.
 # NOTE: `_lbeta` and `_beta` are implemented for explictly test purpose.
-_lbeta(α, β) = lgamma(α) + lgamma(β) - lgamma(α + β)
-_beta(α, β) = exp(_lbeta(α, β))
+# TODO: implement this broadcasting in Knet.jl
+_lbeta(α, β) = lgamma.(α) + lgamma.(β) - lgamma.(α + β)
+_beta(α, β) = exp.(_lbeta(α, β))
 lbeta(α, β) = _lbeta(α, β)
 beta(α, β) = _beta(α, β)
 
