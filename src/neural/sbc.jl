022caf0b8ac1715b483a5514e6967c280166af85
Kai Xu <xukai921110@gmail.com>
2019-04-21 17:00:13 +0100

    fix type mismatch

--- src/neural/sbc.jl
+++ src/neural/sbc.jl
@@ -31,8 +31,8 @@ function StructuredSBC(ibp::IBP{T}, i_dim::Int, k_init::Int=0; kwargs...) where 
 end
 
 function StructuredSBC(i_dim::Int, k_init::Int=0, α::AbstractFloat=one(FT), β::AbstractFloat=one(FT), ; kwargs...)
-    a = Knet.Param(AT(zeros(k_init, 1) .+ log(exp(α) - 1)))
-    b = Knet.Param(AT(zeros(k_init, 1) .+ log(exp(β) - 1)))
+    a = Knet.Param(AT(zeros(FT, k_init, 1) .+ log(exp(α) - 1)))
+    b = Knet.Param(AT(zeros(FT, k_init, 1) .+ log(exp(β) - 1)))
     gumbel = GumbelBernoulliLogitLazyDense(i_dim, k_init; kwargs...)
     return StructuredSBC(a, b, gumbel)
 end
@@ -40,7 +40,8 @@ end
 function (enc::StructuredSBC)(i, d::Int...)
     # NOTE: for non-RR (i.e. static) version, d[1] is always equal to size(a, 1) and size(b, 1)
     # TODO: implement online initialisation
-    dist_kuma = BatchKumaraswamy(softplus.(enc.a[1:d[1],:]) .+ 0.1, softplus.(enc.b[1:d[1],:]) .+ 0.1)
+    dist_kuma = BatchKumaraswamy(softplus.(enc.a[1:d[1],:]) .+ one(FT) / 10,
+                                 softplus.(enc.b[1:d[1],:]) .+ one(FT) / 10)
     dist_gumbel = enc.gumbel(i, d...)
     return dist_kuma, dist_gumbel
 end
