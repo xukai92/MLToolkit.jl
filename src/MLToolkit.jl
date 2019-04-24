4dfd1fc24a62236dbe6c114a7678ba178af657c5
Kai Xu <xukai921110@gmail.com>
2019-04-24 20:14:47 +0100

    add tbX

--- src/MLToolkit.jl
+++ src/MLToolkit.jl
@@ -7,6 +7,8 @@ import AutoGrad, Knet, PyCall, PyPlot, Distributions, Reexport, Distributed
 
 # Python bindings
 const axes_grid1 = PyCall.pyimport("mpl_toolkits.axes_grid1")
+const tbX = pyimport("tensorboardX")
+export tbX
 
 # Constants that are exported
 const FT = Float32
