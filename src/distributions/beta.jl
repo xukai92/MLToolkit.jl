3a52b967ed863fff1509632219160c75e5ae748f
Kai Xu <xukai921110@gmail.com>
2019-04-28 15:13:07 +0100

    improve numerical stability for kuma sample

--- src/distributions/beta.jl
+++ src/distributions/beta.jl
@@ -15,7 +15,7 @@ end
 function _u2logkumaraswamysample(T, u, kuma::BatchKumaraswamy)
     _one = one(T)
     _eps = eps(T)
-    return log.(_one .- exp.(log.(u) ./ kuma.b)) ./ kuma.a
+    return log.(_one .- exp.(log.(u .+ _eps) ./ kuma.b)) ./ kuma.a
 end
 
 """
