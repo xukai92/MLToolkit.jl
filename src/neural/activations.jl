ee8116b01ca2161878f146efba3d19eb5ccf51b4
Kai Xu <xukai921110@gmail.com>
2019-04-28 22:28:34 +0100

    fix typo

--- src/neural/activations.jl
+++ src/neural/activations.jl
@@ -1,7 +1,7 @@
 # TODO: implement CUDA kernels for below
 
 log1pexp(x) = log1p(exp(x))
-logexpm1(x) = log(-expm1(x))
+logexpm1(x) = log(expm1(x))
 
 const softplus = log1pexp
 const invsoftplus = logexpm1
