4238ced88ee539cc1f10a1a8ee8d6cfc0b141e26
Kai Xu <xukai921110@gmail.com>
2019-05-10 00:27:16 +0100

    move get_features_large to MLToolkit

--- src/Data/Data.jl
+++ src/Data/Data.jl
@@ -10,6 +10,6 @@ export BatchDataLoader
 include("visualisation.jl")
 export make_imggrid
 include("features.jl")
-export get_features_griffiths2011indian
+export get_features_griffiths2011indian, get_features_large
 
 end # module
