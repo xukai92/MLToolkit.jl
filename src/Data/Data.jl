1c395f8f6665517a4e3e4d72302745a7ddf23f94
Kai Xu <xukai921110@gmail.com>
2019-10-24 23:27:00 +0100

    update Dataset and DataLoader

--- src/Data/Data.jl
+++ src/Data/Data.jl
@@ -1,13 +1,94 @@
 module Data
 
-using ..MLToolkit: FT
-export FT
+import Random, Distributions, MLDataUtils
 
-# include("mnist.jl")
-# export load_mnist
-include("loader.jl")
-export BatchDataLoader, shuffle!
 include("features.jl")
 export get_features_griffiths2011indian, get_features_large
+
+struct Ring{T<:AbstractFloat} <: Distributions.ContinuousMultivariateDistribution
+    n_clusters::Int
+    s::Int
+    σ::T
+end
+
+function makemixturemodel(ring::Ring)
+    π_typed = convert(typeof(ring.σ), π)
+    cluster_indices = collect(0:ring.n_clusters-1)
+    base_angle = π_typed * 2 / ring.n_clusters
+    angle = (base_angle .* cluster_indices) .- π_typed / 2
+    μ = [ring.s * cos.(angle) ring.s * sin.(angle)]'
+    return MixtureModel([MvNormal(μ[:,i], ring.σ) for i in 1:size(μ, 2)])
+end
+
+Distributions.rand(rng::Random.AbstractRNG, ring::Ring{T}, n::Int) where {T} = convert.(T, rand(rng, makemixturemodel(ring), n))
+
+Distributions.logpdf(ring::Ring, x::AbstractArray{<:AbstractFloat,2}) = logpdf(makemixturemodel(ring), x)
+
+### Dataset and data loader
+
+## Dataset
+
+struct Dataset
+    name::String
+    train
+    test
+    validation
+    function Dataset(train, test=nothing, validation=nothing; name::String="")
+        for set in (train, test, validation)
+            if !isnothing(set)
+                if set isa Tuple
+                    # Length consistency
+                    @assert reduce(==, map(t -> last(size(t)), set))
+                    # Dim consistency
+                    @assert train isa Tuple # if `set` is `Tuple`, `train` must be
+                    @assert length(train) == length(set)
+                    for i in 1:length(train)
+                        @assert Base.front(size(train[i])) == Base.front(size(set[i]))
+                    end
+                else
+                    # Dim consistency
+                    @assert Base.front(size(train)) == Base.front(size(set))
+                end
+            end
+        end
+        return new(name, train, test, validation)
+    end
+end
+
+dim(d::Dataset) = Base.front(size(d.train))
+
+## Data loader
+
+struct DataLoader
+    data::Dataset
+    batch_size::Int
+    batch_size_eval::Int
+    function DataLoader(data::Dataset, batch_size::Int, batch_size_eval::Int=batch_size)
+        return new(data, batch_size, batch_size_eval)
+    end
+end
+
+selectdata(data::AbstractArray{<:Any,1}, idx) = data[idx]
+selectdata(data::AbstractArray{<:Any,2}, idx) = data[:,idx]
+selectdata(data::AbstractArray{<:Any,3}, idx) = data[:,:,idx]
+selectdata(data::AbstractArray{<:Any,4}, idx) = data[:,:,:,idx]
+selectdata(data::Tuple, idx) = map(d -> selectdata(d, idx), data)
+
+ndata(data) = last(size(data))
+ndata(data::Tuple) = last(size(first(data)))
+
+function Base.getproperty(dl::DataLoader, k::Symbol)
+    if k in (:train, :test, :validation)
+        data = getproperty(dl.data, k)
+        n = ndata(data)
+        batch_size = k == :train ? dl.batch_size : dl.batch_size_eval
+        idx_iterator = Iterators.partition(MLDataUtils.shuffleobs(1:n), batch_size)
+        return ((batch=selectdata(data, idx), idx=idx) for idx in idx_iterator)
+    else
+        getfield(dl, k)
+    end
+end
+
+export Dataset, dim, DataLoader
 
 end # module
--- test/Data/Data.jl
+++ test/Data/Data.jl
@@ -1,63 +1,23 @@
-using Test
-using MLToolkit.Data
+using Test, MLToolkit.Data
 
 @testset "Data" begin
-    # @testset "MNIST" begin
-    #     tr_sz = 100
-    #     te_sz = 200
-    #     for mnist_sym = [:mnist, :fmnist]
-    #         x_tr_sub, y_tr_sub, x_te_sub, y_te_sub = load_mnist(mnist_sym, tr_sz, te_sz; flatten=true)
-    #         @test size(x_tr_sub) == (784, tr_sz)
-    #         @test size(y_tr_sub) == (tr_sz,)
-    #         @test size(x_te_sub) == (784, te_sz)
-    #         @test size(y_te_sub) == (te_sz,)
-
-    #         x_tr_sub, y_tr_sub, x_te_sub, y_te_sub = load_mnist(mnist_sym, tr_sz, te_sz; flatten=false)
-    #         @test size(x_tr_sub) == (28, 28, 1, tr_sz)
-    #         @test size(y_tr_sub) == (tr_sz,)
-    #         @test size(x_te_sub) == (28, 28, 1, te_sz)
-    #         @test size(y_te_sub) == (te_sz,)
-    #     end
-    # end
-
-    @testset "BatchDataLoader" begin
-        sz = 100
-        x = randn(784, sz)
-        y = rand(sz)
-
-        batch_size = 20
-        mnist_loader = BatchDataLoader(batch_size, x, y; drop_last=true)
-        x1, y1 = first(mnist_loader)
-        @test length(mnist_loader) == 5
-        @test size(x1, 2) == batch_size
-        @test size(y1, 1) == batch_size
-
-        mnist_loader = BatchDataLoader(batch_size, x, y; drop_last=false)
-        @test length(mnist_loader) == 5
-
-        batch_size = 30
-
-        mnist_loader = BatchDataLoader(batch_size, x, y; drop_last=false)
-        @test length(mnist_loader) == 4
-
-        mnist_loader = BatchDataLoader(batch_size, x, y; drop_last=true)
-        @test length(mnist_loader) == 3
-
-        at_list = [Array{Float16,2}, Array{Float32,2}, Array{Float64,2}]
-        # if AT == KnetArray
-            # push!(at_list, AT{FT,2})
-        # end
-        for at in at_list
-            mnist_loader = BatchDataLoader(batch_size, x; drop_last=true, atype=at)
-            x1 = first(mnist_loader)
-            @test typeof(x1) == at
-        end
-
-        @warn "`shuffle!()` not tested"
-    end
-
     @testset "get_features" begin
         @warn "`get_features_griffiths2011indian()` not tested"
         @warn "`get_features_large()` not tested"
+    end
+
+    @testset "Dataset and DataLoader" begin
+        n = 100
+        x = randn(784, n)
+        y = rand(n)
+
+        dataset = Dataset((x, y))
+
+        batch_size = 20
+        loader = DataLoader(dataset, batch_size)
+        @test length(loader.train) == 5
+        x1, y1 = first(loader.train).batch
+        @test size(x1, 2) == batch_size
+        @test size(y1, 1) == batch_size
     end
 end
