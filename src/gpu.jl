77186df06eeb02b23c62f2534687f9482c80900e
Kai Xu <xukai921110@gmail.com>
2019-10-25 13:56:41 +0100

    remove code moved to CuArrays

--- src/gpu.jl
+++ src/gpu.jl
@@ -1,16 +1,7 @@
 using CuArrays
-
-CuMatOrAdj{T} = Union{CuMatrix, LinearAlgebra.Adjoint{T, <:CuMatrix{T}}, LinearAlgebra.Transpose{T, <:CuMatrix{T}}}
-CuOrAdj{T} = Union{CuVecOrMat, LinearAlgebra.Adjoint{T, <:CuVecOrMat{T}}, LinearAlgebra.Transpose{T, <:CuVecOrMat{T}}}
+using CuArrays: CuMatOrAdj, CuOrAdj
 
 Base.inv(x::CuMatOrAdj{<:AbstractFloat}) = CuArrays.CUBLAS.matinv_batched([x])[2][1]
-
-# TODO: remove this when my PR for CuArrays is merged
-function Base.:\(_A::CuMatOrAdj, _B::CuOrAdj)
-    A, B = copy(_A), copy(_B)
-    A, ipiv = CuArrays.CUSOLVER.getrf!(A)
-    return CuArrays.CUSOLVER.getrs!('N', A, ipiv, B)
-end
 
 import Base.\
 A::Tracker.TrackedArray \ B::Tracker.TrackedArray = Tracker.track(\, A, B)
