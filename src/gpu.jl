12e2f1d43f3ddbaa1d6ea2ffa75299eec5eb2e5a
Kai Xu <xukai921110@gmail.com>
2019-10-25 02:35:07 +0100

    fix @grad for mat div

--- src/gpu.jl
+++ src/gpu.jl
@@ -1,11 +1,21 @@
 using CuArrays
 
-CuMatOrAdj{T} = Union{CuMatrix, LinearAlgebra.Adjoint{T, CuMatrix{T}}, LinearAlgebra.Transpose{T, CuMatrix{T}}}
-CuOrAdj{T} = Union{CuVecOrMat, LinearAlgebra.Adjoint{T, CuVecOrMat{T}}, LinearAlgebra.Transpose{T, CuVecOrMat{T}}}
+CuMatOrAdj{T} = Union{CuMatrix, LinearAlgebra.Adjoint{T, <:CuMatrix{T}}, LinearAlgebra.Transpose{T, <:CuMatrix{T}}}
+CuOrAdj{T} = Union{CuVecOrMat, LinearAlgebra.Adjoint{T, <:CuVecOrMat{T}}, LinearAlgebra.Transpose{T, <:CuVecOrMat{T}}}
 
 Base.inv(x::CuMatOrAdj{<:AbstractFloat}) = CuArrays.CUBLAS.matinv_batched([x])[2][1]
 
-Tracker.@grad function (A::CuMatOrAdj{T} \ B::CuOrAdj{T}) where {T<:AbstractFloat}
+# TODO: remove this when my PR for CuArrays is merged
+function Base.:\(_A::CuMatOrAdj, _B::CuOrAdj)
+    A, B = copy(_A), copy(_B)
+    A, ipiv = CuArrays.CUSOLVER.getrf!(A)
+    return CuArrays.CUSOLVER.getrs!('N', A, ipiv, B)
+end
+
+A::Trakcer.TrackedArray \ B::Trakcer.TrackedArray = Tracker.track(\, A, B)
+A::CuMatOrAdj           \ B::Trakcer.TrackedArray = Tracker.track(\, A, B)
+A::Trakcer.TrackedArray \ B::CuOrAdj              = Tracker.track(\, A, B)
+Tracker.@grad function (A::Union{CuMatOrAdj,Trakcer.TrackedArray} \ B::Union{CuMatOrAdj,Trakcer.TrackedArray})
     return Tracker.data(A) \ Tracker.data(B), function (Δ)
         ∇A = -(A' \ Δ) * B' / A'
         return (∇A,  (A' \ Δ))
