05b32efef58cc7679ffdc2561f8fa2937e4bf7a0
Kai Xu <xukai921110@gmail.com>
2019-10-25 02:03:16 +0100

    implment grad for \ for cu

--- src/gpu.jl
+++ src/gpu.jl
@@ -1,16 +1,13 @@
 using CuArrays
 
-Base.inv(x::CuArray{<:Real,2}) = CuArrays.CUBLAS.matinv_batched([x])[2][1]
+CuMatOrAdj{T} = Union{CuMatrix, LinearAlgebra.Adjoint{T, CuMatrix{T}}, LinearAlgebra.Transpose{T, CuMatrix{T}}}
+CuOrAdj{T} = Union{CuVecOrMat, LinearAlgebra.Adjoint{T, CuVecOrMat{T}}, LinearAlgebra.Transpose{T, CuVecOrMat{T}}}
 
-function Base.:\(_A::CuArray{<:Real}, _B::CuArray{<:Real})
-    A, B = copy(_A), copy(_B)
-    A, ipiv = CuArrays.CUSOLVER.getrf!(A)
-    return CuArrays.CUSOLVER.getrs!('N', A, ipiv, B)
+Base.inv(x::CuMatOrAdj{<:AbstractFloat}) = CuArrays.CUBLAS.matinv_batched([x])[2][1]
+
+Tracker.@grad function (A::CuMatOrAdj{T} \ B::CuOrAdj{T}) where {T<:AbstractFloat}
+    return Tracker.data(A) \ Tracker.data(B), function (Δ)
+        ∇A = -(A' \ Δ) * B' / A'
+        return (∇A,  (A' \ Δ))
+    end
 end
-
-# Tracker.@grad function (A \ B)
-#     return Tracker.data(A) \ Tracker.data(B), function (Δ)
-#         ∇A = -(A' \ Δ) * B' / A'
-#         return (∇A,  (A' \ Δ))
-#     end
-# end
