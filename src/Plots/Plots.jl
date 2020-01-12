34aa6d3f775e6c9969672264fef75a6e9c71dc44
Kai Xu <xukai921110@gmail.com>
2020-01-12 05:58:19 +0000

    fix image grid

--- src/Plots/Plots.jl
+++ src/Plots/Plots.jl
@@ -40,7 +40,11 @@ Usage:
 fig = plot(p)
 ```
 """
-plot(p::AbstractPlot)  = throw(MethodError(plot,  p))
+function plot(p::AbstractPlot, args...; kwargs...)
+    fig, ax = plt.subplots()
+    plot!(ax, p, args...; kwargs...)
+    return fig
+end
 
 """
     plot!([ax=plt.gca()], p::AbstractPlot)
@@ -52,7 +56,7 @@ plot!(ax, p)
 ```
 """
 plot!(ax, p::AbstractPlot) = throw(MethodError(plot!, p))
-plot!(p::AbstractPlot; kwargs...) = plot!(plt.gca(), p; kwargs...)
+plot!(p::AbstractPlot, args...; kwargs...) = plot!(plt.gca(), p, args...; kwargs...)
 
 """
     get_tikz_code([fig], p::AbstractPlot; kwargs...)
@@ -117,7 +121,51 @@ function autoset_lims!(ax, x)
 end
 autoset_lims!(x) = autoset_lims!(plt.gca(), x)
 
-export autoget_lims, autoset_lims!
+"""
+    make_imggrid(img::AbstractArray{<:Number,4}, n_rows, n_cols; gap::Int=1)
+
+Create a n_rows by n_cols image grid.
+"""
+function make_imggrid(img::AbstractArray{<:Number,4}, n_rows, n_cols; gap::Int=1)
+    w, h, c, n = size(img)
+    @assert c == 1 || c == 3
+    imggrid = 0.5 * ones(n_rows * (w + gap) + gap, n_cols * (h + gap) + gap, c)
+    i = 1
+    for row = 1:n_rows, col = 1:n_cols
+        if i <= n
+            i_row = (row - 1) * (w + gap) + 1
+            i_col = (col - 1) * (h + gap) + 1
+            imggrid[i_row+1:i_row+w,i_col+1:i_col+h,:] .= img[:,:,:,i]
+        else
+            break
+        end
+        i += 1
+    end
+    if c == 1  # imshow supports gray as 3D tensor
+        imggrid = dropdims(imggrid; dims=3)
+    end
+    return imggrid
+end
+
+function make_imggrid(img::AbstractArray{<:Number,3}, n_rows, n_cols; gap::Int=1)
+    w, h, n = size(img)
+    return make_imggrid(reshape(img, w, h, 1, n), n_rows, n_cols; gap=gap)
+end
+
+"""
+    make_imggrid(img; gap::Int=1)
+
+Create an approximately squared image grid based on the number of images.
+For example, if last(size(img)) is 100, the grid will be 10 by 10.
+"""
+function make_imggrid(img; gap::Int=1)
+    n = last(size(img))
+    n_rows = ceil(Int, sqrt(n))
+    n_cols = n_rows * (n_rows - 1) > n ? n_rows - 1 : n_rows
+    return make_imggrid(img, n_rows, n_cols; gap=gap)
+end
+
+export autoget_lims, autoset_lims!, make_imggrid
 
 ### Plots
 
@@ -135,6 +183,6 @@ function plot_actmat!(Z::Matrix; ax=plt.gca())
     return ax
 end
 
-export TwoYAxesLines, GrayImages, RGBImages, ContourFunction, plot_actmat!
+export TwoYAxesLines, ImageGrid, ContourFunction, plot_actmat!
 
 end # module
--- test/Plots/Plots.jl
+++ test/Plots/Plots.jl
@@ -1,42 +1,63 @@
-using Test, LaTeXStrings, Distributions
+using Test, LaTeXStrings, MLDatasets, Distributions
 using MLToolkit.Plots
 
-@testset "make_imggrid" begin
-    d = 5
-    rand_img = rand(d^2, 100)
-    n_rows = n_cols = 3
-    gap = 1
-    imggrid = make_imggrid(rand_img, n_rows, n_cols)
-    @test all(imggrid[1,:] .== 0.5)
-    @test all(imggrid[end,:] .== 0.5)
-    @test all(imggrid[:,1] .== 0.5)
-    @test all(imggrid[:,end] .== 0.5)
-    @test size(imggrid) == (
-        n_rows * d + gap * (n_rows + 1),
-        n_cols * d + gap * (n_cols + 1)
-    )
+@testset "Utilites" begin
+    @testset "autoget_lims" begin
+        x = [0 1; 0 1]
+        xlims, ylims, zlims = autoget_lims(x)
+        @test xlims == [-0.1, 1.1]
+        @test ylims == [-0.1, 1.1]
+        @test zlims == nothing
+
+        x = [0 1; 0 1; 0 1]
+        xlims, ylims, zlims = autoget_lims(x)
+        @test xlims == [-0.1, 1.1]
+        @test ylims == [-0.1, 1.1]
+        @test zlims == [-0.1, 1.1]
+    end
+
+    @testset "make_imggrid" begin
+        d = 5
+        rand_img = rand(d, d, 100)
+        n_rows = n_cols = 3
+        gap = 1
+        imggrid = make_imggrid(rand_img, n_rows, n_cols)
+        @test all(imggrid[1,:] .== 0.5)
+        @test all(imggrid[end,:] .== 0.5)
+        @test all(imggrid[:,1] .== 0.5)
+        @test all(imggrid[:,end] .== 0.5)
+        @test size(imggrid) == (
+            n_rows * d + gap * (n_rows + 1),
+            n_cols * d + gap * (n_cols + 1)
+        )
+    end
 end
 
-@testset "plot and save" begin
-    # Two axes lines
-    x = collect(1:0.1:10)
-    y1 = sin.(x)
-    y2 = x .^ 2
+@testset "Plots" begin
+    @testset "TwoYAxesLines" begin
+        x = collect(1:0.1:10)
+        y1 = sin.(x)
+        y2 = x .^ 2
 
-    p = TwoYAxesLines(x, y1, y2)
-    fig = plot(p, "--"; xlabel="x", ylabel1=L"\sin(x)", ylabel2=L"x^2")
+        p = TwoYAxesLines(x, y1, y2)
+        fig = plot(p, "--"; xlabel="x", ylabel1=L"\sin(x)", ylabel2=L"x^2")
 
-    savefig(fig, p, "test.tex")
-    savefig(fig, p, "test.png"; bbox_inches="tight")
+        savefig(fig, p, "two_y_axes_lines.tex")
+        savefig(fig, p, "tetwo_y_axes_lines.png"; bbox_inches="tight")
+    end
 
-    # # Images
-    # imgs = GrayImages(rand(16, 16, 10))
+    @testset "ImageGrid" begin
+        for (dataset, x) in [
+            ("mnist", reshape(permutedims(MNIST.traintensor(Float32, 1:100), (2, 1, 3)), 784, :)),
+            ("cifar10", permutedims(CIFAR10.traintensor(Float32, 1:100), (2, 1, 3, 4))),
+        ]
+            p = ImageGrid(x)
+            fig = plot(p)
 
-    # plt.figure()
-    # plot!(imgs)
-
-    # plt.savefig("test_gray_images.pdf", bbox_inches="tight")
-
+            savefig(fig, p, "imagegrid_$dataset.tex")
+            savefig(fig, p, "imagegrid_$dataset.png"; bbox_inches="tight")
+        end
+    end
 
     # # Two-dimensional density
     # p = plot(MvNormal(zeros(2), 1), (-3, 3), (-3, 3))
