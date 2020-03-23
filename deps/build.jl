7b27f0a987bcf908f663f71ba52121398d06543d
Kai Xu <xukai921110@gmail.com>
2020-03-23 23:16:28 +0000

    add tikzplot

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,6 @@
-run(`pip install matplotlib`)
+PIP = replace(read(`which pip`, String), "\n" => "")
+println("Install Python dependencies using $PIP")
+run(`pip install matplotlib tikzplotlib`)
 PYTHON = replace(read(`which python`, String), "\n" => "")
 ENV["PYTHON"] = PYTHON
 using Pkg
