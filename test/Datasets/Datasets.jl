fab72da8dbae03a8e83632c37708b6f614a7e269
Kai Xu <xukai921110@gmail.com>
2020-03-24 17:13:18 +0000

    simplify test

--- test/Datasets/Datasets.jl
+++ test/Datasets/Datasets.jl
@@ -1,12 +1,12 @@
 using Test, MLToolkit.Datasets
 
-@testset "Neural" begin
-    tests = [
-        # "architecture.jl",
-    ]
-    foreach(include, tests)
+@testset "Datasets" begin
+    IGNORES = ["mnist", "cifar10"]
 
     for name in Datasets.DATASET_NAMES
+        name in IGNORES && continue
         dataset = Dataset(name, 30_000)
     end
+
+    @warn "Datasets in $IGNORES are not tested."
 end
